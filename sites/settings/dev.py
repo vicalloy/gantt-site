8cba8b5b002165db5eee3dac1753640c29dc7512
vicalloy <zbirder@gmail.com>
2012-12-21 16:57:34 +0800

    init project

---
+++ sites/settings/dev.py
@@ -0,0 +1,22 @@
+from base import *
+
+DEBUG = True
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+#debug_toolbar
+try:
+    import debug_toolbar
+    INSTALLED_APPS += ('debug_toolbar',)
+    MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+    INTERNAL_IPS = ('127.0.0.1',)
+except Exception, e:
+    pass
+
+#devserver
+try:
+    import devserver
+    INSTALLED_APPS += ('devserver',)
+    MIDDLEWARE_CLASSES += ('devserver.middleware.DevServerMiddleware',)
+    INSTALLED_APPS = ('devserver',) + INSTALLED_APPS#devserver must in first
+except Exception, e:
+    pass
